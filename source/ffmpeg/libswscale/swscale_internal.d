c83af29f751337847799cd137b34f91be685be73
donglei@donglei.com <donglei@donglei.com>
2015-09-16 13:59:17 +0800

    ffmpeg swscale lib for d extern

---
+++ source/ffmpeg/libswscale/swscale_internal.d
@@ -0,0 +1,32 @@
+module ffmpeg.libswscale.swscale_internal;
+
+import ffmpeg.libswscale.swscale_version;
+import ffmpeg.libavutil.avutil;
+
+extern(C):
+enum YUVRGB_TABLE_HEADROOM = 256;
+
+//enum MAX_FILTER_SIZE = SWS_MAX_FILTER_SIZE;
+
+
+static if( AV_HAVE_BIGENDIAN)
+ enum ALT32_CORR = (-1);
+else
+	enum ALT32_CORR =  1;
+
+static if( ARCH_X86_64){
+	enum APCK_PTR2 = 8;
+	enum APCK_COEF = 16;
+	enum APCK_SIZE = 24;
+}
+else{
+	enum APCK_PTR2 = 4;
+	enum APCK_COEF  = 8;
+	enum APCK_SIZE = 16;
+}
+
+enum RETCODE_USE_CASCADE = -12345;
+
+
+
+
